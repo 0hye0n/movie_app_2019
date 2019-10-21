34203be01c456efd918016777207c1c8bb7e53fa
dolkaiser <43217783+dolkaiser@users.noreply.github.com>
2019-10-22 04:13:16 +0900

    2019-10-22

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from "./Movie";
+import { async } from 'q';
 
 class App extends React.Component
 {
@@ -7,19 +9,39 @@ class App extends React.Component
     isLoading: true,
     movies: []
   };
-  //6sec
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
-    axios
+  //이 함수가 약간의 시간의 걸리므로 끝날때까지 기다려 달라고 지정해주는것 async
+  getMovies = async () =>{
+    //await를 통해 정확히 어떤걸 기다려하 되는지 지정
+    const {
+      data: {
+        data :{movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //둘다 똑같은 동작
+    //this.setState({movies: movies});
+    this.setState({movies, isLoading: false});
   }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
   render(){
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
 
     return (
       <div>
-       {this.state.isLoading ? "Loading" : "We are ready"}
+       {this.state.isLoading ? "Loading...." : movies.map(movie => {
+         console.log(movie);
+         return (<Movie 
+                    key={movie.id}
+                    id={movie.id} 
+                    year={movie.year} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image}/>
+         );
+       })}
       </div> 
     )
   }
